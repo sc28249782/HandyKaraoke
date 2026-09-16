0459311d0a1355bad45371974e6fd0ca5ef89d3c
Somchai Pongkasem <sc28249782@gmail.com>
2026-09-16 22:16:26 +0700

    feat: back up settings before resetting them

--- Config.cpp
+++ Config.cpp
@@ -1,6 +1,9 @@
 #include "Config.h"
 
 #include <QApplication>
+#include <QDateTime>
+#include <QFile>
+#include <QFileInfo>
 
 QString Config::DATABASE_DIR_PATH       = ALL_DATA_DIR_PATH + "/Data";
 QString Config::DATABASE_FILE_PATH      = Config::DATABASE_DIR_PATH + "/Database.db3";
@@ -23,4 +26,68 @@ void Config::initConfigDataPath()
         CONFIG_APP_FILE_PATH = CONFIG_DIR_PATH + "/HandyKaraoke.conf";
         CONFIG_SYNTH_FILE_PATH = CONFIG_DIR_PATH + "/SynthMixer.conf";
     }
+}
+
+
+void Config::enableSafeMode()
+{
+    QDir dir(TEMP_DIR_PATH + "/safe-mode");
+    if (!dir.exists())
+        dir.mkpath(".");
+
+    CONFIG_APP_FILE_PATH = dir.filePath("HandyKaraoke.conf");
+    CONFIG_SYNTH_FILE_PATH = dir.filePath("SynthMixer.conf");
+
+    // Do not allow a previous safe-mode session to become another source of
+    // startup problems.
+    QFile::remove(CONFIG_APP_FILE_PATH);
+    QFile::remove(CONFIG_SYNTH_FILE_PATH);
+}
+
+bool Config::resetSettings(QString *backupDirectory, QString *errorMessage)
+{
+    QDir configDir(CONFIG_DIR_PATH);
+    if (!configDir.exists() && !configDir.mkpath(".")) {
+        if (errorMessage)
+            *errorMessage = QString("Cannot create configuration directory: %1")
+                    .arg(CONFIG_DIR_PATH);
+        return false;
+    }
+
+    const QString stamp = QDateTime::currentDateTime()
+            .toString("yyyyMMdd-HHmmsszzz");
+    const QString backupPath = configDir.filePath("backup-" + stamp);
+    const QStringList files = {
+        CONFIG_APP_FILE_PATH,
+        CONFIG_SYNTH_FILE_PATH
+    };
+
+    bool movedAnyFile = false;
+    for (const QString &filePath : files) {
+        if (!QFile::exists(filePath))
+            continue;
+
+        if (!QDir().mkpath(backupPath)) {
+            if (errorMessage)
+                *errorMessage = QString("Cannot create backup directory: %1")
+                        .arg(backupPath);
+            return false;
+        }
+
+        const QString destination = QDir(backupPath)
+                .filePath(QFileInfo(filePath).fileName());
+        if (!QFile::rename(filePath, destination)) {
+            if (errorMessage)
+                *errorMessage = QString("Cannot back up configuration file: %1")
+                        .arg(filePath);
+            return false;
+        }
+
+        movedAnyFile = true;
+    }
+
+    if (backupDirectory)
+        *backupDirectory = movedAnyFile ? backupPath : QString();
+
+    return true;
 }
